bcf98896f0576312ea9fc9c38c3b0e6d33cbee1f
Taylor Galloway <gallowaytaylor@outlook.com>
2021-04-22 21:45:03 -0500

    Add functionality to switch from choose game view to classic mode

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //DOM elements
 
-var humanWins = document.querySelector('#humanWins')
-var computerWins = document.querySelector('#computerWins')
-
+var humanWins = document.querySelector('#humanWins');
+var computerWins = document.querySelector('#computerWins');
+var classicModeButton = document.querySelector('#classicModeButton');
+var chooseGameView = document.querySelector('.choose-game');
+var classicModeView = document.querySelector('.classic-mode');
 
 //Global Variables
 var currentGame = new Game();
@@ -10,11 +12,17 @@ var currentGame = new Game();
 // currentGame.playGame(2);
 
 //Event Listeners
-window.addEventListener('click', runSample);
+// window.addEventListener('click', runSample);
+classicModeButton.addEventListener('click', showMode);
 
 //Event Handlers
 function runSample() {
   currentGame.playGame(3);
+}
+
+function showMode() {
+  hide(chooseGameView);
+  show(classicModeView);
 }
 
 //Other Functions
@@ -26,4 +34,12 @@ function getRandomNumber() {
 function displayWins(player1Wins, player2Wins) {
   humanWins.innerText = `Wins: ${player1Wins}`;
   computerWins.innerText = `Wins: ${player2Wins}`;
+}
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+function hide(element) {
+  element.classList.add('hidden');
 }
